b80c23b30c30381f1f30ca564ebcba584fca26a6
Adam Ring <adam@thelastbyte.com>
2020-10-16 17:01:28 -0400

    Including admin and system email vars

--- php/config.php
+++ php/config.php
@@ -6,6 +6,15 @@
  * 
  */
 
+/** Recipient of system emails */
+define('ADMIN_EMAIL', 'adam@thelastbyte.com');
+
+/** The systems email's from address */
+define('FROM_ADDRESS', 'moneywarp@thelastbyte.com');
+
+/** System email's from Name */ 
+define('FROM_NAME', 'Money Warp');
+
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
@@ -18,6 +27,12 @@ define('DB_USER', 'SDEV-435');
 /** MySQL database password */
 define('DB_PASSWORD', 'Put in your own password here.');
 
+/**
+ * System vars, don't modify anything below this line.
+ */
+
+/** Combining email vars from above for nice email formatting */
+define('FORMATED_FROM', 'From: "'. FROM_NAME .'" <'. FROM_ADDRESS .'>');
 
 /*
  * Making sure that the PHP session cookie is
