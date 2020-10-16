bff95fbedda1eeb2767f32905ca29992942042a0
Adam Ring <adam@thelastbyte.com>
2020-10-16 16:39:55 -0400

    Initial testing of some basic authorization

--- php/login.php
+++ php/login.php
@@ -4,23 +4,35 @@
  */
 require_once 'includePHP.php';
 
-
+/** Initiate the Sessions class */
 $mySession = new ikanspelwel\MySessions();
 
-//$mySession->Login('10');
-//$mySession->Logout();
+/**
+ * Since we are trying to login, we will logout
+ * any other active session, so we can start clean
+ * and fresh.
+ */
+$mySession->Logout();
+
+/** Connecting to the Db */
+try {
+    $db = new ikanspelwel\MyDatabase();
+    
+    // Prepare Statement
+    $userCheck = $db->get_dbh()->prepare( 'SELECT COUNT(*) FROM `user` WHERE `user_name` = :user_name AND `password` = :password' );
+    
+    // Execute Statement
+    $userCheck->execute( array(':user_name' => $_REQUEST['user_name'], ':password' => $_REQUEST['password']) );
+    
+    /** Get the results */
+    list($validUser) = $userCheck->fetch(PDO::FETCH_NUM);
+    
+} catch (Exception $e) {
+    mail(ADMIN_EMAIL, 'MySQL PDO Error', $e->getMessage() .' in '. __FILE__ ." on line ". __LINE__, EMAIL_FROM, '-f'. EMAIL_FROM);
+    exit;
+}
 
 echo "<pre>";
 echo PHP_EOL;
-if( $mySession->Active() ){
-    echo 'Valid/Active Session - User ID: '. $_SESSION['userId'] . PHP_EOL;
-} else {
-    echo 'Invalid/Inactive Session'. PHP_EOL;
-}
-echo PHP_EOL;
-echo DB_NAME .PHP_EOL;
-echo DB_USER .PHP_EOL;
-echo DB_PASSWORD .PHP_EOL;
-echo DB_HOST .PHP_EOL;
+echo $validUser . PHP_EOL;
 echo "</pre>";
-
