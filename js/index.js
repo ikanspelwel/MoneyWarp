9c72d81fe8414b0b45aa70f6b9f32447d301d1c1
Adam Ring <adam@thelastbyte.com>
2020-10-25 11:56:16 -0400

    Updated index page to use an ajax call for cleaner UI interaction

--- js/index.js
+++ js/index.js
@@ -28,10 +28,64 @@ $( document ).ready(function() {
 			 * the main home page and the logout button
 			 */
 			$('#homeDiv').show();
-			$('#logout').show();
+			$('#logout').removeClass('invisible');
 		}
 
 		/** Now we can show the main body. */
 		$('body').show();
 	});
+	
+	/**
+	 * Binding the submit action on the login form
+	 * so we can handle it with an ajax/json call.
+	 */
+	$('#loginForm').on('submit', function() {
+		/** Call the fucntion to process the login */
+		ProcessLogin();
+
+		/** Return False to stop the normal form processing */
+		return false;
+	});
+	
 });
+
+
+/**
+ * Function to process a login.
+ * 
+ * @returns void
+ */
+function ProcessLogin() {
+
+	/** Calling the login form via ajax */
+	$.ajax({
+		url: 'php/login.php',
+		type: 'POST',
+		cache: 'false',
+		data: {
+			user_name: $('#user_name').val(),
+			password: $('#password').val()
+		}
+	})
+	.fail(function(jqXHR, textStatus, errorThrown) {
+		/** Something odd happend, normally this should never happen. */
+		alert('A system error has occurred, please refresh and try again. If this error persists please report it.');
+	})
+	.done(function(json) {
+		if(json.error) {
+			/** If there was an error, show it. */
+			Attention.show(json.error);
+			return;
+		}
+		
+		/** Else no error proceed */
+		if(json.success) {
+			/** If the login was successfull we will refresh this page */
+			window.location.href = './';
+		} else {
+			/** Invalid login, so error message */
+			Attention.show('Invalid user name or password.');
+		}
+
+	})
+}
