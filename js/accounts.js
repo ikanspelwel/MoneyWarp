7ebd48e8d1902369277399099b46ecc815bc71c1
Adam Ring <adam@thelastbyte.com>
2020-11-06 16:57:28 -0500

    Wrapped up bank accounts add/edit/display.

--- js/accounts.js
+++ js/accounts.js
@@ -9,10 +9,12 @@ var Session; 	// Global placeholder for our Session Class
  * after the DOM has been loaded
  */
 $( document ).ready(function() {
+	/** Initialize my JS Classes */
 	Wait = new MySpinner('MySpinner');
 	Attention = new MyAttention('Modal-Attention');
 	Session = new MySessions();
 
+	/** Show the spinning wheel */
 	Wait.show();
 	
 	/**
@@ -32,6 +34,7 @@ $( document ).ready(function() {
 			/** Hide the spinning wheel */
 			Wait.hide();
 			
+			/** Showing the error */
 			Attention.show( 'Your session has expired, please log back in.', {onClose: 'Logout'} );
 			return;
 		} else {
@@ -40,11 +43,22 @@ $( document ).ready(function() {
 			 * now show the main body.
 			 */
 			
+			/** Turn the Add/Edit into a Pop Up */
+			$('#AccountManagement').dialog({
+				autoOpen: false
+			}); // end of new dialog box
+
+			
+			/** Bind the Add icon */
+			$('#AccountList .fa-plus-circle').on('click', function() {
+				DisplayAddEdit();
+			});
+			
 			/** Show the main body */
 			$('body').show();
 			
 			/** Populate the list of accounts */
-			LoadAccounts();
+			ShowAccounts();
 		}
 	});
 	
@@ -59,7 +73,7 @@ $( document ).ready(function() {
  * 
  * @returns void
  */
-function LoadAccounts() {
+function ShowAccounts() {
 	
 	/** Calling the accounts php via ajax */
 	$.ajax({
@@ -98,10 +112,12 @@ function LoadAccounts() {
 						$('<div>').text(data.aba),
 						$('<div>').addClass('text-center').html(
 							$('<i>').addClass('fa').addClass(
-								(data.active ? 'fa-check-square-o' : 'fa-times-circle')
-							).on('click', function() { Wait.show(); })
+								(data.active ? 'fa-check-circle' : 'fa-times-circle')
+							)
 						),
-						$('<div>').html($('<i>').addClass('fa fa-pencil-square-o').on('click', function() { Wait.hide(); }) )
+						$('<div>').html(
+							$('<i>').addClass('fa fa-pencil-square-o').on('click', function() { DisplayAddEdit(data.account_id); })
+						)
 					)
 				);
 			}); /** End of jQuery.each */
@@ -115,5 +131,164 @@ function LoadAccounts() {
 		Wait.hide();
 		
 	})
+}
 
+/**
+ * Function to display the account Add or Edit
+ * window. This function will will open up a new
+ * popup window and either populate it with the
+ * info provided via the account_id or it will
+ * just present a empty form to fill out.
+ * 
+ * @param account_id
+ * @returns void
+ */
+function DisplayAddEdit(account_id) {
+	/** Checking to see if the acount_id was provided or not */
+	if(account_id === undefined) {
+		/** 
+		 * New Account. Will clear the fields set the title
+		 * and show the dialog box.
+		 */
+
+		/** Reset all the fields */
+		$('#description').val('');
+		$('#type').val('0');
+		$('#account_num').val('');
+		$('#aba').val('');
+		$('#active').val('1');
+		
+		/** Set the dialog box title */
+		$('#AccountManagement').dialog( "option", "title", "Add Account" );
+		
+		/** Set the dialog buttons */
+		$('#AccountManagement').dialog( "option", "buttons", {
+			"Add": function() { DoAddUpdate(); },
+			"Cancel": function() { $(this).dialog('close'); }
+		});
+		
+		/** Now show the Dialog Box */
+		$('#AccountManagement').dialog('open');
+		
+	} else {
+		/**
+		 *  Existing account, we get the account deatils
+		 *  from the db and then preform and update. 
+		 */
+		
+		/** Show the spinning wheel */
+		Wait.show();
+
+		/** Calling the accounts php via ajax */
+		$.ajax({
+			url: 'php/accounts.php',
+			type: 'POST',
+			cache: 'false',
+			data: {
+				doWhat: 'getAccount',
+				account_id: account_id
+			}
+		})
+		.fail(function(jqXHR, textStatus, errorThrown) {
+			/** Something odd happend, normally this should never happen. */
+			alert('A system error has occurred, please refresh and try again. If this error persists please report it.');
+		})
+		.done(function(json) {
+			if(json.error) {
+				/** If there was an error, show it. */
+				Attention.show( json.error, {onClose: (json.loggedOut ? 'Logout' : '' )} );
+				return;
+			}
+			
+			/** Else no errors proceed */
+			if(json.data == null) {
+				/** If there aren't elements in the return array */
+				Attention.show( 'That account couldn\'t be found. Please refresh and try again.' );
+				return;
+			} else {
+				/** If there are elements in the return array */
+
+				/** Fill in the returned Values */
+				$('#description').val(json.data.description);
+				$('#type').val(json.data.type);
+				$('#account_num').val(json.data.account_num);
+				$('#aba').val(json.data.aba);
+				$('#active').val(json.data.active);
+
+				/** Set the dialog box title */
+				$('#AccountManagement').dialog( "option", "title", "Update Account" );
+				
+				/** Set the dialog buttons */
+				$('#AccountManagement').dialog( "option", "buttons", {
+					"Update": function() { DoAddUpdate(account_id) },
+					"Cancel": function() { $(this).dialog('close'); }
+				});
+
+				/** Now show the Dialog Box */
+				$('#AccountManagement').dialog('open');
+				
+			} /** End of if there were elements in the data array */
+
+			/** Hide the spinning wheel */
+			Wait.hide();
+			
+		}); /** End of ajax ".done()" */
+		
+	}
+}
+
+
+/**
+ * Function to actually process the add or update. 
+ * This will call the PHP handler, via ajax to
+ * preform the heavy listing and Db interaction
+ *  
+ * @param account_id
+ * @returns void
+ */
+function DoAddUpdate(account_id) {
+	
+	/** Calling the accounts php via ajax */
+	$.ajax({
+		url: 'php/accounts.php',
+		type: 'POST',
+		cache: 'false',
+		data: {
+			doWhat: 'addUpdate',
+			account_id: account_id,
+			description: $('#description').val(),
+			type: $('#type').val(),
+			account_num: $('#account_num').val(),
+			aba: $('#aba').val(),
+			active: $('#active').val()
+		}
+	})
+	.fail(function(jqXHR, textStatus, errorThrown) {
+		/** Something odd happend, normally this should never happen. */
+		alert('A system error has occurred, please refresh and try again. If this error persists please report it.');
+	})
+	.done(function(json) {
+		if(json.error) {
+			/** If there was an error, show it. */
+			Attention.show( json.error, {onClose: (json.loggedOut ? 'Logout' : '' )} );
+			return;
+		}
+		
+		/** Else no errors proceed */
+		if(json.data == null) {
+			/** If there aren't elements in the return array */
+			Attention.show( 'That account couldn\'t be found. Please refresh and try again.' );
+			return;
+		} else {
+			/** Add/Update was successful, refresh page */
+			
+			location.reload();
+			
+		} /** End of if there were elements in the data array */
+
+		/** Hide the spinning wheel */
+		Wait.hide();
+		
+	}); /** End of ajax "done" */
+	
 }
--- php/accounts.php
+++ php/accounts.php
@@ -13,6 +13,9 @@ $myReturn = new ikanspelwel\MyReturnJson();
 /** Initiate my error message class */
 $errors = new ikanspelwel\MyErrorMsg();
 
+/** Initiate my loging error class */
+$debug = new ikanspelwel\MyLog();
+
 /**
  * First verifying they have an active session
  * if they don't then we cannot proceed and we
@@ -39,15 +42,171 @@ switch ( $doWhat ) {
     case 'getAccounts':
         GetAccounts($_REQUEST);
         break;
+    case 'addUpdate':
+        AddUpdate($_REQUEST);
+        break;
+    case 'getAccount':
+        GetAccount($_REQUEST);
+        break;
     default:
         /** If an action couldn't be found, toss an exception message. */
         throw new Exception('Unknown/Invalid option!');
 }
 
 /**
- * Function to return if there is an active session or not.
- * It will be returned via json so that javascript can
- * easially parse it.
+ * Function to add or update an account in the
+ * database. We will first check to make sure all
+ * the provided values are valid and then we will
+ * attempt to update the database. 
+ *
+ * @param array $vars passing the $_REQUEST vars in
+ */
+function AddUpdate($vars) {
+    /** Making my global var accessible to this function */
+    global $mySession, $myReturn, $errors, $debug;
+    
+    /** Var to hold all the data */
+    $data = array();
+    
+    /** Removing white spaces from beginning and end of fields */
+    foreach(array('description', 'account_num', 'aba') AS $eachVar) {
+        $vars[$eachVar] = trim($vars[$eachVar]);
+    }
+    
+    /** Checking to make sure a Nickname was provided */
+    if($vars['description'] == "") {
+        $errors->ErrorMsg('Please provided a name for this account');
+    }
+    
+    /**
+     * Type should be either Checking or Savings
+     */
+    if($vars['type'] != 'Checking' && $vars['type'] != 'Savings') {
+        $errors->ErrorMsg('Invalid account type');
+    }
+
+    /**
+     * Account number and ABA can only contain numbers
+     */
+    if(!preg_match("/^\d+$/", $vars['account_num']) ) {
+        $errors->ErrorMsg('Account number can only contain numbers');
+    }
+    if(!preg_match("/^\d+$/", $vars['aba']) ) {
+        $errors->ErrorMsg('Routing number can only contain numbers');
+    }
+
+    /**
+     * Sanitizing Active Var
+     */
+    $vars['active'] = ($vars['active'] == '1');
+    
+    
+    /** All Checks have been performed */
+    
+    /** Checking our error var for any errors */
+    if(!$errors->ErrorMsg()) {
+        
+        /** No error have been reported, okay to update database */
+        try {
+            /** Initiate my database class */
+            $db = new ikanspelwel\MyDatabase();
+            
+            if($vars['account_id']) {
+                /** If the account_id has been provided it is an updated */
+
+                /** Prepare Statement */
+                $updateAccount = $db->get_dbh()->prepare( 'UPDATE `account` SET `description` = :description, `type` = :type, `account_num` = :account_num, `aba` = :aba, `active` = :active WHERE `user_id` = :user_id AND `account_id` = :account_id' );
+                
+                /** Execute Statement */
+                $updateAccount->execute( array(':user_id' => $_SESSION['user_id'], ':description' => $vars['description'], ':type' => $vars['type'], ':account_num' => $vars['account_num'], ':aba' => $vars['aba'], ':active' => $vars['active'], ':account_id' => $vars['account_id']) );
+                
+            } else {
+                /** No account_id means it is an add */
+
+                /** Prepare Statement */
+                $addAccount = $db->get_dbh()->prepare( 'INSERT INTO `account` (`user_id`, `description`, `type`, `account_num`, `aba`, `active`) VALUES(:user_id, :description, :type, :account_num, :aba, :active)' );
+
+                /** Execute Statement */
+                $addAccount->execute( array(':user_id' => $_SESSION['user_id'], ':description' => $vars['description'], ':type' => $vars['type'], ':account_num' => $vars['account_num'], ':aba' => $vars['aba'], ':active' => $vars['active']) );
+
+                /** Checking for affected rows */
+                if($addAccount->rowCount() != 1) {
+                    /**
+                     * No rows were inserted, so that user_id/account_id
+                     * must have been invaild. Going to report a error,
+                     * However the user is probably trying to do something
+                     * bad because this shouldn't ever happen in normal
+                     * operation.
+                     */
+                    $errors->ErrorMsg('Unable to add new account.');
+                }
+                
+            }
+            
+        } catch (Exception $e) {
+            /** If any exception occurred report it */
+            mail(ADMIN_EMAIL, 'MySQL PDO Error', $e->getMessage() .' in '. __FILE__ ." on line ". __LINE__, FORMATED_FROM, '-f'. FROM_ADDRESS);
+            $myReturn->json( array('error' => "A system error has occurred, please refresh and try again. If this error persists please report it.") );
+            exit;
+        }
+        
+    }
+    
+    /** Returning data via json */
+    $myReturn->json( array('data' => $data, 'error' => $errors->getString('<br/> - ')) );
+}
+
+/**
+ * Function to return an account via the provided
+ * account_id. It will be returned via json so that 
+ * javascript can easially parse it.
+ * 
+ * @param array $vars passing the $_REQUEST vars in
+ */
+function GetAccount($vars) {
+    /** Making my global var accessible to this function */
+    global $mySession, $myReturn, $errors, $debug;
+    
+    /** Var to hold the data */
+    $data = NULL;
+    
+    /** Need to get the account out of the Database */
+    try {
+        /** Initiate my database class */
+        $db = new ikanspelwel\MyDatabase();
+        
+        /** Prepare Statement */
+        $getAccount = $db->get_dbh()->prepare( 'SELECT * FROM `account` WHERE `user_id` = :user_id AND `account_id` = :account_id' );
+        
+        /** Execute Statement */
+        $getAccount->execute( array(':user_id' => $_SESSION['user_id'], ':account_id' => $vars['account_id']) );
+        
+        /** Get the results */
+        if($reference = $getAccount->fetch(PDO::FETCH_ASSOC)) {
+            
+            /** Massaging Data */
+            $reference['active'] += 0; /** Converting to Numeric */
+            
+            /** Storing each returned row in the data var */
+            $data = $reference;
+            
+        }
+        
+    } catch (Exception $e) {
+        /** If any exception occurred report it */
+        mail(ADMIN_EMAIL, 'MySQL PDO Error', $e->getMessage() .' in '. __FILE__ ." on line ". __LINE__, FORMATED_FROM, '-f'. FROM_ADDRESS);
+        $myReturn->json( array('error' => "A system error has occurred, please refresh and try again. If this error persists please report it.") );
+        exit;
+    }
+    
+    /** Returning data via json */
+    $myReturn->json( array('data' => $data, 'error' => $errors->getString()) );
+}
+
+/**
+ * Function to return all the accounts associated with
+ * this user. They will be returned via json so that 
+ * javascript can easially parse it.
  * 
  * @param array $vars passing the $_REQUEST vars in
  */
